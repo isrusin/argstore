196b013069cd6723146b9ab09e09c3f2dc9208e5
isrusin <10874317+isrusin@users.noreply.github.com>
2019-01-28 13:10:37 +0300

    Add root wrapper class, touch docstrings

--- metargparse.py
+++ metargparse.py
@@ -1,7 +1,7 @@
-"""Command-line parsing module with support of metadata dumping
+"""Command-line parsing module with support of argument dumping
 
-This is argparse wrapping for dumping of execution options
-and argument values to the output files.
+This is argparse wrapping which keeps execution options and arguments
+and writes them to the output text files.
 """
 
 __version__ = "0.1"
@@ -18,35 +18,75 @@ from argparse import (Action, ArgumentDefaultsHelpFormatter,
 )
 
 
-class ArgumentParser(object):
-    """Argument parser class.
+class ArgumentContainerWrapper(object):
+    """Root wrapper of the argument containers."""
 
-    Replacement of the argparse.ArgumentParser that wraps the class and
-    stores additional info during argument specification to format the
-    output metadata.
-    """
+    meta_title = "\n"
+    meta_desc = "\n"
+    meta_epilog = "\n"
+    meta_args = None
+    meta_groups = None
 
-    def __init__(self, *args, metatitle=None, metadesc=None, **kwargs):
-        self.parser = _ArgumentParser(*args, **kwargs)
-        self._meta_strs = []
-        if metatitle is None:
-            metatitle = "### %s\n" % (self.parser.prog or sys.argv[0])
-        self._meta_strs.append(metatitle)
-        if metadesc is None:
-            metadesc = "\n"
-        self._meta_strs.append(metadesc)
+    def __init__(self, subj, meta_title=None, meta_desc=None,
+                 meta_epilog=None, meta_args=None, meta_groups=None):
+        self.subj = subj
+        self.meta_title = self._updattr("meta_title", meta_title)
+        self.meta_desc = self._updattr("meta_desc", meta_desc)
+        self.meta_epilog = self._updattr("meta_epilog", meta_epilog)
+        self.meta_args = self._updattr("meta_args", meta_args, [])
+        self.meta_groups = self._updattr("meta_groups", meta_groups, [])
 
-    def add_argument(self, *args, metamsg=None, **kwargs):
-        _argument = self.parser.add_argument(*args, **kwargs)
-        if metamsg is None:
-            metamsg = " {%s}" % _argument.dest
-        self._meta_strs.append(metamsg)
+    def add_argument(self, *args, meta_name=None, meta_str=None,
+                     **kwargs):
+        _argument = self.subj.add_argument(*args, **kwargs)
+        if meta_str is None:
+            if meta_name is None:
+                meta_name = _argument.dest
+            meta_str = " %s: {%s}\n" % (meta_name, _argument.dest)
+        self.meta_args.append(meta_str)
         return _argument
 
-    def add_argument_group(self, *args, metamsg="{}", **kwargs):
-        self._meta_strs.append(metamsg)
-        return self.parser.add_argument_group(*args, **kwargs)
+    def add_argument_group(self, *args, meta_title=None, meta_name=None,
+                           meta_desc=None, **kwargs):
+        _group = self.subj.add_argument_group(*args, **kwargs)
+        if meta_title is None:
+            if meta_name is None:
+                meta_name = _group.title or ""
+            meta_title = " --- %s ---\n" % meta_name
+        group = ArgumentContainerWrapper(
+            _group, meta_title=meta_title, meta_desc=meta_desc
+        )
+        self.meta_groups.append(group)
+        return group
+
+    def _update_value(self, name, value, default=None):
+        if value is None:
+            try:
+                value = self.__getattribute__(name)
+            except AttributeError:
+                pass
+        if value is None:
+            value = default
+        self.__setattr__(name, value)
+
+
+class ArgumentParser(ArgumentContainerWrapper):
+    """Argument parser class.
+
+    Replacement of the argparse.ArgumentParser that wraps it and
+    stores metadata during argument addition.
+    """
+
+    def __init__(self, *args, meta_title=None, meta_desc=None,
+                 meta_epilog=None, **kwargs):
+        subj = _ArgumentParser(*args, **kwargs)
+        if meta_title is None:
+            meta_title = "### %s\n" % (subj.prog or sys.argv[0])
+        super().__init__(
+            subj, meta_title=meta_title, meta_desc=meta_desc,
+            meta_epilog=meta_epilog
+        )
 
     def parse_args(self, *args, **kwargs):
-        return self.parser(*args, **kwargs)
+        return self.subj.parse_args(*args, **kwargs)
 
