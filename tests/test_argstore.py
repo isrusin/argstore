7f6ed46dd8d22ca15d10bf93b26616d0eec99b18
isrusin <10874317+isrusin@users.noreply.github.com>
2019-02-08 01:22:34 +0300

    Add parser parents not implemented test

--- tests/test_argstore.py
+++ tests/test_argstore.py
@@ -1,5 +1,6 @@
 """Tests for argstore."""
 
+import pytest
 import sys
 
 import argstore
@@ -14,4 +15,8 @@ def test_parser_creation_all_defaults():
     assert parser.meta_groups is not None
     assert parser.meta_formatters is not None
     assert parser.subj is not None
+
+def test_parser_parents_not_implemented():
+    with pytest.raises(NotImplementedError):
+        _parser = argstore.ArgumentParser(parents=[])
 
