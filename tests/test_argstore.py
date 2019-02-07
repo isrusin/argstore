175bf445cf60b9778b28cf5e65a9dc76513957af
isrusin <10874317+isrusin@users.noreply.github.com>
2019-02-08 01:21:51 +0300

    Add parser creation test

--- tests/test_argstore.py
+++ tests/test_argstore.py
@@ -1,17 +1,17 @@
-#! /usr/bin/env python3
-
 """Tests for argstore."""
 
 import sys
 
 import argstore
 
-
-def main():
+def test_parser_creation_all_defaults():
     parser = argstore.ArgumentParser()
-    args = parser.parse_args()
-
-
-if __name__ == "__main__":
-    sys.exit(main())
+    assert parser.meta_tag is not None
+    assert parser.meta_title is not None
+    assert parser.meta_desc is not None
+    assert parser.meta_epilog is not None
+    assert parser.meta_args is not None
+    assert parser.meta_groups is not None
+    assert parser.meta_formatters is not None
+    assert parser.subj is not None
 
