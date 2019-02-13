af1c8797eb5c9ee3b0323d712e877942d0960f5c
isrusin <10874317+isrusin@users.noreply.github.com>
2019-02-13 17:09:17 +0300

    Add __init__ tests for ArgumentContainerWrapper

--- tests/test_argstore.py
+++ tests/test_argstore.py
@@ -21,6 +21,46 @@ class _ArgumentContainerStub(object):
         return self.add_argument_group(*args, **kwargs)
 
 
+class TestArgumentContainerWrapper(object):
+    def test_init_defaults(self):
+        wrapper = argstore.ArgumentContainerWrapper(
+            _ArgumentContainerStub()
+        )
+        assert wrapper.meta_tag is not None
+        assert wrapper.meta_title is not None
+        assert wrapper.meta_desc is not None
+        assert wrapper.meta_epilog is not None
+        assert wrapper.meta_args is not None
+        assert wrapper.meta_groups is not None
+        assert wrapper.meta_formatters is not None
+
+    def test_init_meta_kwargs(self):
+        meta_tag = "meta_tag"
+        meta_title = "meta_title"
+        meta_desc = "meta_desc"
+        meta_epilog = "meta_epilog"
+        meta_args = ["meta_args", ]
+        meta_groups = ["meta_groups", ]
+        meta_formatters = ["meta_formatters", ]
+        wrapper = argstore.ArgumentContainerWrapper(
+            _ArgumentContainerStub(),
+            meta_tag=meta_tag,
+            meta_title=meta_title,
+            meta_desc=meta_desc,
+            meta_epilog=meta_epilog,
+            meta_args=meta_args,
+            meta_groups=meta_groups,
+            meta_formatters=meta_formatters
+        )
+        assert wrapper.meta_tag == meta_tag
+        assert wrapper.meta_title == meta_title
+        assert wrapper.meta_desc == meta_desc
+        assert wrapper.meta_epilog == meta_epilog
+        assert wrapper.meta_args is meta_args
+        assert wrapper.meta_groups is meta_groups
+        assert wrapper.meta_formatters is meta_formatters
+
+
 def test_parser_creation_all_defaults():
     parser = argstore.ArgumentParser()
     assert parser.meta_tag is not None
